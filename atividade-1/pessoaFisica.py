199de496e45fc0aaf2ab0901bde680a51150e012
andrefkruger <andrefilipekruger@hotmail.com>
2023-11-15 21:22:38 -0300

    updade pessoas 2

--- atividade-1/pessoaFisica.py
+++ atividade-1/pessoaFisica.py
@@ -4,3 +4,4 @@ class PessoaFisica(Pessoa):
     def __init__(self, nome, telefone, endereço, data_nascimento, cpf):
         super().__init__(nome, telefone, endereço)
         self.data_nascimento = data_nascimento
+        self.cpf = cpf 
