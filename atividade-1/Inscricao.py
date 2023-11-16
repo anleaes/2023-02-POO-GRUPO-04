314db2eeb08fd4b15a39f6ecbe5baa76f7e9f1b7
rafael Pinto Ferreira <rafaelpokm@gmail.com>
2023-11-15 21:07:36 -0300

    update-inscriçao

--- atividade-1/Inscricao.py
+++ atividade-1/Inscricao.py
@@ -1,10 +1,6 @@
 class Inscricao:
-    def __init__(self):
-        self.alunos = []
-    
-    def __str__(self):
-        return f"Nome: {self.alunos}"
-    
-    def inscrever_aluno(self, aluno, curso):
-        self.alunos.append({"aluno": aluno, "curso": curso})
-    pass
+    def __init__(self, numero_matricula, data_matricula, cursos, aluno_matriculado, contratante):
+        self.numero_matricula = numero_matricula
+        self.data_matricula = data_matricula
+        self.curso = cursos
+        self.aluno_matriculado = aluno_matriculado
