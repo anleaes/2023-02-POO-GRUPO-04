9cefc1bef556c334135f01a05f87fe7e89798ff1
MatheusFRRR <matheusfrpi98@gmail.com>
2023-11-16 18:19:50 -0300

    update-inscricao

--- atividade-1/Inscricao.py
+++
@@ -1,7 +0,0 @@
-class Inscricao:
-    def __init__(self, numero_matricula, data_matricula, cursos, aluno_matriculado, contratante):
-        self.numero_matricula = numero_matricula
-        self.data_matricula = data_matricula
-        self.curso = cursos
-        self.aluno_matriculado = aluno_matriculado
-        self.contratante = contratante
