5a87ecf9c80ddd1b891ad3e39ebce54c735da35c
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-11-28 19:12:20 -0300

    Create-Meta-Models

--- Pessoa/models.py
+++ Pessoa/models.py
@@ -7,5 +7,10 @@ class Pessoa(models.Model):
     idade = models.IntegerField()
     cpf = models.CharField(max_length=14, unique=True)
 
+    class Meta:
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+        ordering =['id']
+
     def __str__(self):
         return self.nome
