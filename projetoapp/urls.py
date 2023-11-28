4a1f725f1581a40ca4684007a0b8b9373524c3fb
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-11-28 20:03:41 -0300

    path-projetoapp

--- projetoapp/urls.py
+++ projetoapp/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pessoa/', include('Pessoa.urls', namespace='pessoas')),  
+    path('pessoa/', include('Pessoa.urls', namespace='pessoas')),
+    path('profissionalSaude/', include('categories.urls', namespace='profissionalSaude')), 
     
 ]
