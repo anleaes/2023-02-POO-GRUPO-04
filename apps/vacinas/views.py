4a6af7e41e4fbce84aa1133ff1b60e4136aeb6c0
MatheusFRRR <matheusfrpi98@gmail.com>
2023-11-28 20:48:16 -0300

    create-vacina-view

--- apps/vacinas/views.py
+++ apps/vacinas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Vacina
+from rest_framework import viewsets
+from .serializer import VacinaSerializer
+
+# Após o comentario "# Create your views here." e crie as views "Vacina".
+
+class VacinaViewSet(viewsets.ModelViewSet):
+    queryset = Vacina.objects.all()
+    serializer_class = VacinaSerializer  
