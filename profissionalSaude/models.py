df2365533c2bbf61dc3d587a81e8d6ceb62e27dd
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-11-28 19:41:01 -0300

    Model-profissionlSaude

--- profissionalSaude/models.py
+++ profissionalSaude/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ProfissionalSaude(models.Model):
+    nome = models.CharField(max_length=100)
+    especialidade = models.CharField(max_length=100)
+    registro = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
