cd4028305d5eccd0055ca1c36837e5fb45e0c02e
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-11-28 19:09:31 -0300

    Create-Models-Pessoa

--- Pessoa/models.py
+++ Pessoa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    cpf = models.CharField(max_length=14, unique=True)
+
+    def __str__(self):
+        return self.nome
