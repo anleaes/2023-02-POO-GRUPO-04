16e3bcaa268a9c5da5766de14798f2f6f29e8a18
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-11-28 19:30:51 -0300

    Create-URL

--- Pessoa/urls.py
+++ Pessoa/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 app_name = 'Pessoa'
 
 router = routers.DefaultRouter()
-router.register('', views.PessoaViewSet, basename='pessoa')
+router.register('', views.PessoaViewSet, basename='Pessoa')
 
 urlpatterns = [
     path('', include(router.urls) )
