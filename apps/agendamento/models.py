63ae36f1350c153f4fcd2f7c805fb108bddc7dd5
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-12-04 21:55:32 -0300

    create-agendamento

---
+++ apps/agendamento/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from vacinas.models import Vacina
+from Pessoa.models import Pessoa
+
+#Create your models here.
+class RegistroVacinação(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    vacina = models.ForeignKey(Vacina, on_delete=models.CASCADE)
+    data = models.DateField()
