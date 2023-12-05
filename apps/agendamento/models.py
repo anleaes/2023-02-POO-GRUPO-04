471983706ac72437deedc82c792ebe7244a29c73
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-12-04 21:58:51 -0300

    create-serializer

--- apps/agendamento/models.py
+++ apps/agendamento/models.py
@@ -3,7 +3,7 @@ from vacinas.models import Vacina
 from Pessoa.models import Pessoa
 
 #Create your models here.
-class RegistroVacinação(models.Model):
+class Agendamento(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     vacina = models.ForeignKey(Vacina, on_delete=models.CASCADE)
     data = models.DateField()
