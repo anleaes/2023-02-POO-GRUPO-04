2ece61768ceab53acdeab2d8a2107486ce0b196b
Eduardo Maciel Néris <eduardomneris@gmail.com>
2023-12-04 22:04:55 -0300

    create-views

--- apps/agendamento/views.py
+++ apps/agendamento/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Agendamento
+from rest_framework import viewsets
+from .serializer import AgendamentoSerializer
+
+# Após o comentario "# Create your views here." e crie as views "Agendamento".
+
+class AgendamentoViewSet(viewsets.ModelViewSet):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer  
