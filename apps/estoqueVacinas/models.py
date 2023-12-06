bb492a6ba331fca791601842f5052e58cfc68745
“rafaelokm” <rafaelpokm@gmail.com>
2023-12-06 19:17:12 -0300

    create-models-ev

--- apps/estoqueVacinas/models.py
+++ apps/estoqueVacinas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class EstoqueVacinas(models.Model):
+    vacina = models.ForeignKey(Vacina, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    data_validade = models.DateField()
