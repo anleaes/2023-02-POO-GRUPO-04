d7edea90b2c04242533266a66164cefafd2387d9
“rafaelokm” <rafaelpokm@gmail.com>
2023-12-06 18:59:27 -0300

    localvacinasmodel

--- apps/localVacinacao/models.py
+++ apps/localVacinacao/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class LocalVacinaçao(models.Model):
+    nome = models.CharField(max_length=100)
+    endereco = models.TextField()
+    capacidade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
